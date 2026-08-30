56987b40da00e82499546d0a45a8e9a3317e60ee
Nima <n.afzalpour@gmail.com>
2026-08-30 11:22:52 +0330

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.AVAN_CONFIG = Object.freeze({
   supabaseUrl: 'https://dkyqsxnllvxypigxpygo.supabase.co',
   supabasePublishableKey: 'sb_publishable_Ga779VstO62_e9Dh_GRmfA_yWNa0waz',
-  environment: 'staging'
+  environment: 'production'
 });
