3366e7460c6ee470d62d913f24f8a167f6d95be9
Nima <n.afzalpour@gmail.com>
2026-08-30 22:27:03 +0330

    Gate C-6.2 Production Password Recovery UI

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX='avan-prod-core-';
-const CACHE='avan-prod-core-1-0-v7';
+const CACHE='avan-prod-core-1-0-v8';
 const ASSETS=['./','./index.html','./styles.css','./config.js','./cloud.js','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(CACHE_PREFIX)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
