ad9fc4762bc54e774a0612431e775c8a56739f63
Nima <n.afzalpour@gmail.com>
2026-09-04 19:47:45 +0330

    RC1 Deploy Core 1.0 To Staging

--- avan-staging/config.js
+++ avan-staging/config.js
@@ -1,6 +1,11 @@
 window.AVAN_CONFIG = Object.freeze({
   supabaseUrl: 'https://dkyqsxnllvxypigxpygo.supabase.co',
-  supabasePublishableKey: 'sb_publishable_Ga779VstO62_e9Dh_GRmfA_yWNa0waz',
-  environment: 'staging-d1',
-  authRedirectUrl: 'https://afzalpour.github.io/avan-staging/'
+
+  supabasePublishableKey:
+    'sb_publishable_Ga779VstO62_e9Dh_GRmfA_yWNa0waz',
+
+  environment: 'staging-rc1',
+
+  authRedirectUrl:
+    'https://afzalpour.github.io/avan-staging/'
 });
