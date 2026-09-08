1769fefb28998e4b06b1e6d657e9a40363c44517
Nima <n.afzalpour@gmail.com>
2026-09-08 16:09:20 +0330

    Load invoice payable total contract

--- avan-staging/rc15-c1-bootstrap.js
+++ avan-staging/rc15-c1-bootstrap.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import './src/ui/tax/tax-date-aware.js';
+import './src/ui/invoices/invoice-payable-total.js';
 import './src/ui/reports/custom-report-builder.js';
 import './src/ui/company/company-logo-file-control.js';
 import './src/ui/localization/persian-runtime-guard.js';
