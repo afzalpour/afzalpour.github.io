eb334ad9602909395f45e6abaaabec746a431023
Nima <n.afzalpour@gmail.com>
2026-09-15 15:58:32 +0330

    Refresh Stock Hunter cache for direct-feed fallback

--- stock-hunter-v4/sw.js
+++ stock-hunter-v4/sw.js
@@ -1,4 +1,4 @@
-const CACHE='shikar-sahm-v4.0.2-20260915';
+const CACHE='shikar-sahm-v4.0.3-20260915';
 const STATIC=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})())});
