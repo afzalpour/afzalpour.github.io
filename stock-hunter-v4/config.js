178e7ba81326222d458840d0e20e23e5e8332e62
Nima <n.afzalpour@gmail.com>
2026-09-16 13:47:29 +0330

    Connect Stock Hunter 4.1.0 to integrated decision view

--- stock-hunter-v4/config.js
+++ stock-hunter-v4/config.js
@@ -1,6 +1,6 @@
 window.STOCK_HUNTER_CONFIG = {
   SUPABASE_URL: 'https://summnepwuziwulzvpcms.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_NYVQwMabwGow-UFVcPx0qw_IvFCIjdr',
-  TABLE: 'stock_hunter_signals_v4',
+  TABLE: 'stock_hunter_integrated_v1',
   REFRESH_MS: 15000
 };
