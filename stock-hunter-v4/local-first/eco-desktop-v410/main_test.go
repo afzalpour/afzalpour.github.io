00f3d156bc8f80012cfd97af58d37741098e2530
Nima <n.afzalpour@gmail.com>
2026-09-25 18:08:30 +0330

    Fix Eco Desktop market-window test registration

--- stock-hunter-v4/local-first/eco-desktop-v410/main_test.go
+++ stock-hunter-v4/local-first/eco-desktop-v410/main_test.go
@@ -13,10 +13,10 @@ func TestMarketWindow(t *testing.T) {
 	}{
 		{"Saturday open", time.Date(2026, 9, 26, 9, 0, 0, 0, tehran), true},
 		{"Friday closed", time.Date(2026, 9, 25, 9, 0, 0, 0, tehran), false},
-		{"before", time.Date(2026, 9, 26, 8, 19, 0, 0, 0, tehran), false},
-		{"start", time.Date(2026, 9, 26, 8, 20, 0, 0, 0, tehran), true},
-		{"end", time.Date(2026, 9, 26, 17, 5, 0, 0, 0, tehran), true},
-		{"after", time.Date(2026, 9, 26, 17, 6, 0, 0, 0, tehran), false},
+		{"before", time.Date(2026, 9, 26, 8, 19, 0, 0, tehran), false},
+		{"start", time.Date(2026, 9, 26, 8, 20, 0, 0, tehran), true},
+		{"end", time.Date(2026, 9, 26, 17, 5, 0, 0, tehran), true},
+		{"after", time.Date(2026, 9, 26, 17, 6, 0, 0, tehran), false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
