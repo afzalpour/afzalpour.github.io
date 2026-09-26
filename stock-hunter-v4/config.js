76c20d36bcc9bf850dbddb3826a1bff070c4bf82
Nima <n.afzalpour@gmail.com>
2026-09-26 09:53:12 +0330

    Stock Hunter: make Supabase the only public live-feed route

--- stock-hunter-v4/config.js
+++ stock-hunter-v4/config.js
@@ -1,7 +1,7 @@
 window.STOCK_HUNTER_CONFIG = {
   SUPABASE_URL: 'https://summnepwuziwulzvpcms.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_NYVQwMabwGow-UFVcPx0qw_IvFCIjdr',
-  LOCAL_BRIDGE_URL: 'http://127.0.0.1:41716',
+  LOCAL_BRIDGE_URL: '',
   TABLE: 'stock_hunter_integrated_v1',
   REFRESH_MS: 15000
 };
